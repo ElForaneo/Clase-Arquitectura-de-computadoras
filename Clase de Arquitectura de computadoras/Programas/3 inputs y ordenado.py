df7e75bc9a36a52321622813908c04552f89b961
Alejandro Rios <alejandro.rios.d.angel@gmail.com>
2022-02-14 00:31:37 -0600

    Update 3 inputs y ordenado.py

--- Clase de Arquitectura de computadoras/Programas/3 inputs y ordenado.py
+++ Clase de Arquitectura de computadoras/Programas/3 inputs y ordenado.py
@@ -1,9 +1,15 @@
 #Ingresar 3 numeros y ordenarlos
 
-lista = []
-lista.append(int(input("Ingrese el primer numero: ")))
-lista.append(int(input("Ingrese el segundo numero: ")))
-lista.append(int(input("Ingrese el tercer numero: ")))
+try:
+    lista = []
+    lista.append(int(input("Ingrese el primer numero: ")))
+    lista.append(int(input("Ingrese el segundo numero: ")))
+    lista.append(int(input("Ingrese el tercer numero: ")))
 
+
+#Esta parte del try y except es del Codigo de Abraham que vimos en clase, Gracias :U
+except ValueError:
+    print("No introdujo un numero awewonao")
+ 
 lista.sort()
 print("Tu lista ordenada:" + " ", lista)
